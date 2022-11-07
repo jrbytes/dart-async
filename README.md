948ece8d5d05eaa0ff8f64902ad51a04a4c8db93
Jr Bytes <itamazor@gmail.com>
2022-11-07 18:09:11 -0300

    init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
+
+## Getting Started
+
+Create a small app
+------------------
+```bash
+$ dart create -t console <dirname>
+```
+
+Run the app
+-----------
+```bash
+$ dart run
+```
+
+## Installing packages
+Site: pub.dev
+
+Install a package
+-----------------
+```bash
+$ dart pub add <package>
+```
---
+++ lib/01-async-api-map.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future example() async {
+  final todo = await fetch();
+  print(todo.toJson());
+}
+
+Future<Todo> fetch() async {
+  var url = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
+  var response = await http.get(url);
+  var json = jsonDecode(response.body);
+  var todo = Todo.fromJson(json);
+  return todo;
+}
+
+class Todo {
+  final String title;
+  final int id;
+  final int userId;
+  final bool completed;
+
+  Todo({
+    required this.title,
+    required this.id,
+    required this.userId,
+    required this.completed,
+  });
+
+  factory Todo.fromJson(Map json) {
+    return Todo(
+      id: json['id'],
+      title: json['title'],
+      userId: json['userId'],
+      completed: json['completed'],
+    );
+  }
+
+  Map toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'userId': userId,
+      'completed': completed,
+    };
+  }
+}
